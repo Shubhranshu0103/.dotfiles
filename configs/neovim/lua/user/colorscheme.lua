b70ae6eac33b204cd4fc7bdcdbb359a2ecdc2e51
shubhranshu0103 <shubhranshusingh.work@gmail.com>
2023-05-13 20:21:40 +0530

    Refactoring

--- configs/neovim/lua/user/colorscheme.lua
+++ configs/neovim/lua/user/colorscheme.lua
@@ -1,7 +1,10 @@
-local colorscheme = "darkplus"
+return {
+  setup = function(colorscheme)
+    local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+    if not status_ok then
+      vim.notify("colorscheme " .. colorscheme .. " not found!")
+      return
+    end
+  end
+}
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
