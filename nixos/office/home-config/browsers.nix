1a9685eb5d1ac220c7ddd803653da6c7bf58ad6e
ShubhranshuSingh <shubhranshusingh.singh@juspay.in>
2023-09-01 11:05:42 +0530

    Added quteBrowser config

--- nixos/office/home-config/browsers.nix
+++ nixos/office/home-config/browsers.nix
@@ -1,9 +1,12 @@
 { config, pkgs, ... }:
-{
+let 
+  qtconfig = builtins.readFile ../../../configs/qutebrowser/config.py;
+in {
 
   programs.qutebrowser = {
     enable = true;
     loadAutoconfig = true;
+    extraConfig = qtconfig;
   };
 
 }
